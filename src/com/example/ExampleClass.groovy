3904d58950a7a54d0ae1280c153e5da4623950b8
Nathan Duckett <nduckett13@gmail.com>
2021-02-13 16:23:10 +1300

    Add example test

--- src/com/example/ExampleClass.groovy
+++ src/com/example/ExampleClass.groovy
@@ -4,4 +4,8 @@ class ExampleClass {
     static def printMessageToJenkins(Object script) {
         script.echo("Hello from inside ExampleClass")
     }
+
+    static def add(int num1, int num2) {
+        return num1 + num2
+    }
 }
---
+++ test/com/example/ExampleClassTests.groovy
@@ -0,0 +1,12 @@
+package com.example
+
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.*
+
+class ExampleClassTests {
+    @Test
+    void testAdd() {
+        assertEquals(4, ExampleClass.add(2, 2))
+    }
+}
